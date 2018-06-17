8ce07b96bb6a75e1866bfb708ee3f6e1a4d29b22
Basant kumar Hota <basahota@users.noreply.github.com>
2018-06-17 16:35:43 +0530

    #code

---
+++ src/main/java/com/javatechie/social/api/SpringFacebookExampleApplication.java
@@ -0,0 +1,14 @@
+package com.javatechie.social.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFacebookExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFacebookExampleApplication.class, args);
+	}
+	
+
+}
